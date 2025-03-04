fd204714a1945bc6cd04d7cf0499034cbca64b00
ZinHua-Li <kadeksetiadharma108@gmail.com>
2025-03-03 20:02:49 -0800

    Pembaruan Sistem Quest
    Membaruan system sceen

--- code/Materi/Materilevel1/materi_1.gd
+++ code/Materi/Materilevel1/materi_1.gd
@@ -1,6 +1,17 @@
 extends Control
 
+@onready var button: Button = $Button
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var timer: Timer = $Timer
 
 
 func _on_button_pressed() -> void:
-	
+	Engine.time_scale = 0.99
+	animation_player.play("paperon")
+	timer.start()
+
+
+
+func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
+	get_tree().change_scene_to_file("res://sceen/Materi/materilevel1/materi2.tscn")
