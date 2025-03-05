05d05d77613d7c3904b00efb469980b44952adfe
ZinHua-Li <setiadharma224@gmail.com>
2025-03-05 21:53:39 +0800

    Pembaruan Sistem Dialog
    Pembaruan sistem UI dan Teks

--- code/player.gd
+++ code/player.gd
@@ -9,7 +9,7 @@ const JUMP_VELOCITY = -200.0
 
 
 func  _unhandled_input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("interact"):
 		var actionables = actionable_finder.get_overlapping_areas()
 		if actionables.size() > 0:
 			actionables[0].action()
